d83606221c1a285481e00d831addff3b1a119701
farchettiensis <farchettiensis@users.noreply.github.com>
2024-08-22 16:42:41 -0300

    Inicialização classe Agenda

--- src/br/com/farchettiensis/agendacontatos/Agenda.java
+++ src/br/com/farchettiensis/agendacontatos/Agenda.java
@@ -1,5 +1,28 @@
 package br.com.farchettiensis.agendacontatos;
 
 public class Agenda {
+    private Contato[] contatos;
+    private int contador;
+    private String nome;
 
+    public Agenda() {
+        this.contatos = new Contato[5];
+    }
+
+    public Agenda(String nome) {
+        this.contatos = new Contato[5];
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void adicionarContato(Contato contato) {
+        if (contador < contatos.length) {
+            contatos[contador] = contato;
+            contador++;
+        }
+
+    }
 }
