07cc5c4631f9569b3e94136e8c0ca37e2068701e
farchettiensis <farchettiensis@users.noreply.github.com>
2024-08-22 22:51:02 -0300

    Remoção de contatos; listar contatos, usando bubble sort para ordená-los antes

--- src/br/com/farchettiensis/agendacontatos/Agenda.java
+++ src/br/com/farchettiensis/agendacontatos/Agenda.java
@@ -26,6 +26,67 @@ public class Agenda {
         contador++;
     }
 
+    public void removerContato(String nome) {
+        for (int i = 0; i < contador; i++) {
+            if (contatos[i].getNome().equals(nome)) {
+                removerContatoPorIndice(i);
+                return;
+            }
+        }
+    }
+
+    public void removerContato(Telefone telefone) {
+        for (int i = 0; i < contador; i++) {
+            if (contatos[i].getTelefone().getNumero().equals(telefone.getNumero())) {
+                removerContatoPorIndice(i);
+                return;
+            }
+        }
+    }
+
+    public void removerContato(String nome, Telefone telefone) {
+        for (int i = 0; i < contador; i++) {
+            if (contatos[i].getNome().equals(nome) &&
+                    contatos[i].getTelefone().getNumero().equals(telefone.getNumero())) {
+                removerContatoPorIndice(i);
+                return;
+            }
+        }
+    }
+
+    private void removerContatoPorIndice(int indice) {
+        for (int i = indice; i < contador - 1; i++) {
+            contatos[i] = contatos[i + 1];
+        }
+        contatos[contador - 1] = null;
+        contador--;
+    }
+
+    public void listarContatos() {
+        ordenarContatos();
+
+        for (int i = 0; i < contador; i++) {
+            System.out.printf("%d - %s", i, contatos[i].getNome());
+        }
+    }
+
+    private void ordenarContatos() {
+        for (int i = 0; i < contador - 1; i++) {
+            for (int j = 0; j < contador - 1 - i; j++) {
+                if (contatos[j].getNome().compareTo(contatos[j + 1].getNome()) > 0) {
+                    Contato contatoTemporario = contatos[j];
+                    contatos[j] = contatos[j + 1];
+                    contatos[j + 1] = contatoTemporario;
+                }
+            }
+        }
+    }
+
+
+    public void editarContato() {
+
+    }
+
     private void redimensionarArray() {
         int novoTamanho = contatos.length + (contatos.length / 2);
         if (novoTamanho == contatos.length) {
@@ -37,7 +98,6 @@ public class Agenda {
         }
         contatos = novoArray;
     }
-
 
     public int getTamanhoArray() {
         return contatos.length;
